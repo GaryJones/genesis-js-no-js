bb00b44dcccdb7859c13d90d0e4050bf9d7fb849
Gary Jones <gary@garyjones.co.uk>
2014-08-23 22:01:07 +0100

    Update file-level documentation
    Changes from GPL3 license to GPL2+.
    
    References GitHub repo, including allowing updates from there.

--- genesis-js-no-js.php
+++ genesis-js-no-js.php
@@ -2,25 +2,33 @@
 /**
  * Genesis js / no-js WordPress plugin.
  *
- * For child themes of the Genesis Theme.
+ * For child themes of the Genesis Framework.
+ * 
  * Adds a no-js body class to the front-end, and a script on genesis_before
  * which immediately changes the class to js if JavaScript is enabled. This is
  * how WP does things on the back-end, to allow different styles for the same
  * elements depending if JavaScript is active or not.
  *
- * @package GenesisJsNoJs
- * @author Gary Jones
- * @license GPL3
+ * @package   GenesisJsNoJs
+ * @author    Gary Jones, Gamajo Tech <gamajo@gamajo.com>
+ * @license   GPL-2.0+
+ * @link      https://github.com/GaryJones/genesis-js-no-js
+ * @copyright 2014 Gary Jones, Gamajo Tech
  *
- * Plugin Name: Genesis js / no-js
- * Plugin URI: http://code.garyjones.co.uk/plugins/genesis-js-no-js/
- * Description: For child themes of the <a href="http://genesis-theme-framework.com/">Genesis Theme</a>. Adds a <code>no-js</code> body class to the front-end, and a script on <code>genesis_before</code> which immediately changes the class to <code>js</code> if JavaScript is enabled. This is how WP does things on the back-end, to allow differing styles for elements if JavaScript is active or not.
- * Version: 1.0.1
- * Author: Gary Jones
- * Author URI: http://garyjones.co.uk/
- * License: GPL3
+ * @wordpress-plugin
+ * Plugin Name:       Genesis js / no-js
+ * Plugin URI:        https://github.com/GaryJones/genesis-js-no-js
+ * Description:       For child themes of the <a href="http://genesis-theme-framework.com/">Genesis Framework</a>. Adds a <code>no-js</code> body class to the front-end, and a script on <code>genesis_before</code> which immediately changes the class to <code>js</code> if JavaScript is enabled. This is how WP does things on the back-end, to allow differing styles for elements if JavaScript is active or not.
+ * Version:           1.0.1
+ * Author:            Gary Jones
+ * Author URI:        http://gamajo.com/
+ * Text Domain:       genesis-js-no-js
+ * Domain Path:       /languages
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * GitHub Plugin URI: https://github.com/GaryJones/genesis-js-no-js
+ * GitHub Branch:     master
  */
-
 
 /**
  * Plugin class for Genesis js / no-js
